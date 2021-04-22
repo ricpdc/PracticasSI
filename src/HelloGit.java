d7f8c9f0480d0b735f32708df46d8d69ca2d2ffb
BatuTala2000 <50117855+BatuTala2000@users.noreply.github.com>
2021-04-22 10:52:53 +0200

    Añadido Hola Mundo

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -4,7 +4,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!!!");
 		System.out.println("test1");
-
+		System.out.println("Hola Mundo");
 	}
 
 }
