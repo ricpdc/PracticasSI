2d3cae1b80755f9b2af6c3ebdd7f72711d2d0224
david <david@david-PC>
2021-04-22 11:15:06 +0200

    ..

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -4,9 +4,11 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!!!");
 		System.out.println("test1");
-		System.out.print("Daniel Durán");
+		System.out.print("Daniel Durï¿½n");
 		System.out.println("Otro hello");
 		System.out.print("Guillermo Santos");
+		System.out.print("David");
+
 	}
 
 }
