b3c010aa2daeb145fe9bb1fb8443ff7f3b9d641d
Ricardo <Ricardo@169.254.149.25>
2021-04-23 15:39:38 +0200

    pequeño programa para sortear orden de defensa de trabajos de teoría

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,14 +1,37 @@
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git!!!!");
-		System.out.println("test1");
-		System.out.print("Daniel Durï¿½n");
-		System.out.println("Otro hello");
-		System.out.print("Guillermo Santos");
-		System.out.print("David Gr");
-		System.out.print("David");
+		
+		
+		Random random = new Random(System.currentTimeMillis());
+		
+		int numGrupos = 10;
+		
+		List<Integer> citaGrupos = new ArrayList<Integer>();
+		int grupo = 0;
+		String diaCita="";
+		for(int cita=0; cita<numGrupos; cita++) {
+			grupo = random.nextInt(10)+1;
+			while(citaGrupos.contains(grupo)) {
+				grupo = random.nextInt(10)+1;
+			}
+			citaGrupos.add(grupo);
+			if(cita <4) {
+				diaCita="Miércoles 5 de Mayo ";
+			}
+			else if(cita < 8) {
+				diaCita="Miércoles 12 de Mayo";
+			}
+			else {
+				diaCita="Jueves 13 de Mayo   ";
+			}
+			System.out.println(diaCita+" grupo: " + grupo);
+		}
 
 	
 
